6b7ecd6502643131aba0dab9fa9634fba617366e
zoa2944 <zoa2944@orange.com>
2019-10-23 09:05:39 +0200

    [commun] Autorise 0 dans un inventaire.

--- base/views.py
+++ base/views.py
@@ -897,7 +897,7 @@ def inventory(request):
         if form.is_valid():
             for p, q in form.cleaned_data.items():
                 pdt = Product.objects.get(pk=p)
-                if q:
+                if q is not None:
                     diff = q - pdt.stock
                     if diff != 0:  # todo check
                         op = ChangeStockOp.create_inventory(product=pdt, quantity=diff)
